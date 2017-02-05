7186c3226cb6a073c406bc4b9a5a991c490c207e
Mawhy <mmalaj@gmail.com>
2017-02-05 01:28:18 +0100

    Add getAdCustomizerSource

---
+++ getAdCustomizerSource.js
@@ -0,0 +1,9 @@
+function getAdCustomizerSource() {
+  var sources = AdWordsApp.adCustomizerSources().get();
+  while (sources.hasNext()) {
+    var source = sources.next();
+    if (source.getName() == 'Flowers') {
+      Logger.log(source.getName() + ' ' + source.getAttributes());
+    }
+  }
+}
